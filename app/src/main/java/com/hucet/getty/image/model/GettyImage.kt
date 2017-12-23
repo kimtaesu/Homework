ba4cbe3b1a135b9f4edd4fe4160f273b11db3560
taesu <kimtaesoo188@gmail.com>
2017-12-23 16:29:54 +0900

    fix : add baseUrl

--- app/src/main/java/com/hucet/getty/image/model/GettyImage.kt
+++ app/src/main/java/com/hucet/getty/image/model/GettyImage.kt
@@ -4,6 +4,9 @@ package com.hucet.getty.image.model
  * Created by taesu on 2017-12-23.
  */
 data class GettyImage(
-        val url: String,
+        private val _url: String,
         val caption: String
-)
+) {
+    var url = _url
+        get() = "http://www.gettyimagesgallery.com/$_url"
+}
